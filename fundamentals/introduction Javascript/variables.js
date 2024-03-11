962c3878ddc8ed1bebe04648eaf3f2aca73c1295
Daniel Cintori <daniel.camargo.teixeira@gmail.com>
2024-03-11 12:03:33 -0300

    Create variables.js
    data types and variables

---
+++ fundamentals/introduction Javascript/variables.js
@@ -0,0 +1,20 @@
+// Tipos de dados em JavaScript
+// number  = 1, 2, 3
+// string = "Quotes"
+// bolean = yes or no
+// function = block of code 
+// object = Keys and values 
+// undefined = undefined value 
+// null = without a value
+
+
+
+// Há três maneiras de declarar uma variável em JavaScript: 
+
+// Antiga versão com vazamento de escopo
+var userEmail = 'exemple@email.com';
+// Versão nova sem vazamento de escopo
+let userId = 1234;
+// Versão segura de declarar uma variável e não altera-lá
+const userLocation = 'Brasil';
+
