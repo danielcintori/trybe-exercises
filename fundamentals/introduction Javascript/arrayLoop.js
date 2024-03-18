e34d680c0399015b65fc43c39e7f0cb0e7fc41ec
Daniel Cintori <daniel.camargo.teixeira@gmail.com>
2024-03-18 13:01:41 -0300

    Update arrayLoop.js

---
+++ fundamentals/introduction Javascript/arrayLoop.js
@@ -0,0 +1,27 @@
+// const personName = 'Michelle';
+// const personName2 = 'Renan';
+// const personLastName = 'Silva'
+// const personLastName2 = 'Nascimento';
+
+// const personFullName = `${personName} ${personLastName}`;
+// const personFullName2 = `${personName2} ${personLastName2}`;
+
+// console.log(personFullName);
+// console.log(personFullName2);
+// Template literals
+
+// const myName = "Adrian";
+
+// console.log("Hello" + " " + myName);
+// console.log(`Hello ${myName}`);
+
+// const product = "Apple Vision Pro";
+// const price = 4048;
+// const discount = 10;
+// text = `The product ${product} regular price is u$${price} but this weekend with discount you only need to pay ${
+//   price - price * (discount / 100)
+// } `;
+// console.log(text);
+
+const shoppingList= ['Rice', 'Potato', 'Milk', 'Beans'];
+console.log(shoppingList[0]);
