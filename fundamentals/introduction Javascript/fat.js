80aa30b79f4612425dcdc485a12e26b30cd29a6a
Daniel Cintori <daniel.camargo.teixeira@gmail.com>
2024-03-19 09:54:53 -0300

    update
    update

---
+++ fundamentals/introduction Javascript/fat.js
@@ -0,0 +1,100 @@
+// const personName = 'Michelle';
+// const personName2 = 'Renan';
+// const personLastName = 'Silva'
+// const personLastName2 = 'Nascimento';
+// const personFullName = `${personName} ${personLastName}`;
+// const personFullName2 = `${personName2} ${personLastName2}`;
+// console.log(personFullName);
+// console.log(personFullName2);
+// Template literals
+// const myName = "Adrian";
+// console.log("Hello" + " " + myName);
+// console.log(`Hello ${myName}`);
+// const product = "Apple Vision Pro";
+// const price = 4048;
+// const discount = 10;
+// text = `The product ${product} regular price is u$${price} but this weekend with discount you only need to pay ${
+//   price - price * (discount / 100)
+// } `;
+// console.log(text);
+// const shoppingList= ['Rice', 'Potato', 'Milk', 'Beans'];
+// console.log(shoppingList[0]);
+// console.log(shoppingList[1]);
+// shoppingList[1] = 'Watermelon';
+// console.log(shoppingList[1]);
+// push() adicionar elemento ao final do array
+// unshift() adiciona um elemento ao início do array
+// pop() Remove o último elemento do array
+// shift() Remove o primeiro elemento do array 
+// shoppingList.forEach((e) => console.log(e));
+// shoppingList.push('zebra');
+// shoppingList.forEach((e) => console.log(e));
+// shoppingList.unshift('Albatroz');
+// shoppingList.forEach((element) => console.log(element));
+// shoppingList.pop();
+// shoppingList.shift();
+// shoppingList.forEach((element) => console.log(element));
+// console.log(shoppingList.length);
+// const menu = ['Home', 'Serviços', 'Portifólio', 'Links'];
+// let menuServices = '';
+// menuServices = menu[1];
+// console.log(menuServices)
+// menu.push('Contato');
+// console.log(menu)
+// const laps = 65;
+// for( let i = 1; i <= laps; i++){
+//     console.log(`Volta ${i} completada!`);
+// }
+// console.log('Fim da corrida!');
+// //Array de números
+// const numbers = [2,19, 23, 4, 8, 19];
+// //Variáveis complementar que irá acumular os valores somados
+// let sum = 0;
+// //loop for que vai somar tudo;
+// for (let index = 0; index < numbers.length; index += 1) {
+//     sum += numbers[index];
+//     console.log(sum);    
+// }
+// console.log(sum);
+// const cars = ['saab','Volvo', 'BMW', 'Chevrolet'];
+// for(let i = 0; i < cars.length; i += 1){
+//     const currentCar = cars[i];
+//     console.log(currentCar);
+//     console.log(cars[i]);
+// }
+// const groceryLIst = ['Arroz', 'Feijão', 'Alface', 'Melancia', 'Macarrão'];
+// for (let index = 0; index < groceryLIst.length; index++) {
+//     console.log(groceryLIst[index]);
+// }
+// const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27, 200];
+// let sum = 0;
+// numbers.forEach((e) => console.log(e));
+// for (let index = 0; index < numbers.length; index++) {
+//        sum += numbers[index];
+//        console.log(sum);
+// }
+// let media = sum/ numbers.length;
+// console.log(`A media do total ${sum} e o número de elementos ${numbers.length} da a média de : ${media}`);
+// if (media > 20) {
+//     console.log('O valor da média aritmética é maior que 20');
+// }else {
+//     console.log('O valor da média aritmética é menor ou igual a 20');
+// }
+// console.log('Vou descobrir o maior valor dentro do array ' + numbers);
+// let biggest = 0;
+// for (let index = 0; index < numbers.length; index++) {
+//     if(biggest < numbers[index]){
+//         biggest = numbers[index];
+//     }
+//     console.log(biggest);
+// }
+// for (let index = 0; index < numbers.length; index++) {
+//     if(numbers[index] % 2 === 1){
+//         console.log(numbers[index]);
+//     }    
+// }
+// fatorial
+let fat = 3;
+let result;
+for (let i = 0; i < fat; i += 1) {
+}
