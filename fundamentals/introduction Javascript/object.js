879407fcc4973016c3d4f1fd132707548e80d9b3
Daniel Cintori <daniel.camargo.teixeira@gmail.com>
2024-03-22 19:40:02 -0300

    update object

--- fundamentals/introduction Javascript/object.js
+++ fundamentals/introduction Javascript/object.js
@@ -87,29 +87,174 @@
 //   console.log(`A jogadora ${player.name} ${player.lastName} foi eleita a melhor do mundo por ${player['bestInTheWorld'].length} vezes.`)
 
 
-const customer = {
-    firstName: 'Roberto',
-    lastName: 'Silva',
-    age: 22,
-    job: 'Software Engineer'
-};
+// const customer = {
+//     firstName: 'Roberto',
+//     lastName: 'Silva',
+//     age: 22,
+//     job: 'Software Engineer'
+// };
 
 
-const addProperty = (object, key, value) => {
-    if( typeof object[key] === 'undefined'){
-        object[key] = value;
-    } 
-};
+// const addProperty = (object, key, value) => {
+//     if( typeof object[key] === 'undefined'){
+//         object[key] = value;
+//     } 
+// };
 
-addProperty(customer, 'birthPlayce', '19/07/1932');
-addProperty(customer, 'email', 'roberto.dev@email.com');
-addProperty(customer, 'fone', '1599999999');
-addProperty(customer, 'userGithub', 'robertinhodev');
-addProperty(customer, 'linkedIn', 'RS');
+// addProperty(customer, 'birthPlayce', '19/07/1932');
+// addProperty(customer, 'email', 'roberto.dev@email.com');
+// addProperty(customer, 'fone', '1599999999');
+// addProperty(customer, 'userGithub', 'robertinhodev');
+// addProperty(customer, 'linkedIn', 'RS');
 
-console.log(customer);
+// console.log(customer);
 
-customer['naMao'] = 'Espero que entre no valor do objeto';
+// customer['naMao'] = 'Espero que entre no valor do objeto';
 
-console.log(customer);
+// console.log(customer);
 
+// Dedução pensamento computacional
+
+//  verde azul amarelo vermelho  
+
+// Benjamin inocente
+// carlos culpa pedro
+// Mario culpa carlos
+// I Pedro inocenta Mario  
+
+// let age = 20;  
+// let category;  
+// if (age >= 18) {  
+//   category = 'maior de idade';  
+// } else {  
+//   category = 'menor de idade';  
+// };  
+  
+// console.log('A pessoa é ' + category);
+
+// let saldo = 50;  
+// let situacao;  
+
+// if (saldo <= 0) {  
+//     situacao = "Você tem dinheiro na conta";  
+// } else {  
+//     situacao = "Sua conta está negativa";  
+// }
+
+// let saldo = 50;  
+// let situacao;  
+
+// if (saldo < 0) {  
+//     situacao = "Você tem dinheiro na conta";  
+// } else {  
+//     situacao = "Sua conta está negativa";  
+// }
+
+
+// if (saldo > 0) {  
+//     situacao = "Você tem dinheiro na conta";  
+// } else {  
+//     situacao = "Sua conta está negativa";  
+// }
+
+// let saldo = 50;  
+// let situacao;  
+// if (saldo >= 0) {  
+//     situacao = "Você tem dinheiro na conta";  
+// } else {  
+//     situacao = "Sua conta está negativa";  
+// }
+
+// let saldo = 50;  
+// let situacao;  
+
+// if (saldo <= 0) {  
+//     situacao = "Você tem dinheiro na conta";  
+// } else {  
+//     situacao = "Sua conta está negativa";  
+// }
+
+// let saldo = 50;  
+// let situacao;  
+
+// if (saldo < 0) {  
+//     situacao = "Você tem dinheiro na conta";  
+// } else {  
+//     situacao = "Sua conta está negativa";  
+// }
+
+// if (saldo > 0) {  
+//     situacao = "Você tem dinheiro na conta";  
+// } else {  
+//     situacao = "Sua conta está negativa";  
+// }
+
+
+// function inicio() {
+//     contador = 1;
+
+//     while (contador <= 10 ){
+//         console.log(`${contador} `);
+//         contador += 1;
+//     }
+// }
+
+// inicio()
+
+
+// let contador = 0;  
+  
+// while (contador <= 5) {  
+//   console.log("Contagem: " + contador);  
+//   contador++;  
+// }
+
+// let contador = 2;  
+  
+// while (contador <= 5) {  
+//   console.log("Contagem: " + contador);  
+//   contador++;  
+// }
+
+// for (let index = 1; index <= 10; index+=1) {  
+//     if (i % 2 === 0) {  
+//         console.log(i);  
+//     }  
+// }
+
+// for (let i = 1; i <= 10; i++) {  
+//     if (i % 2 !== 0) {  
+//         console.log(i);  
+//     }  
+// }
+
+// let count = 0;  
+  
+// for (let i = 2; i <= 150; i++) {  
+//     if (count === 50) {  
+//         console.log("Mensagem secreta!");  
+//         break;  
+//     }  
+//     if (i % 3 === 0) {  
+//         count++;  
+//     }  
+// }
+
+// let count = 0;  
+  
+// for (let index = 2; index <= 150; index++) {  
+//     if (index % 3 === 0) {  
+//         count++;  
+//     }  
+// }  
+// if (count === 50) {  
+//     console.log("Mensagem secreta!");  
+// }
+
+
+let index = 1;  
+  
+while (index <= 10) {  
+    console.log(index);  
+    index++;  
+}
